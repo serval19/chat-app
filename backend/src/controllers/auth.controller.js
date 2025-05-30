b7a52a5a37dce056bdffc0ec5cf7a5f24fd4d559
serval19 <diwatomy04@gmail.com>
2025-05-30 19:38:21 +0530

    signup logic completely implemented

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,8 +1,12 @@
+import { generateToken } from "../lib/utils.js"
 import User from "../models/user.model.js"
 import bcrypt from 'bcryptjs'
 export const signup = async (req,res)=>{
    const {fullName,email,password}=req.body
    try{
+    if(!fullName || !email || !password){
+        return res.status(400).json({message: "Please fill all the fields"})
+    }
     if(password.length<6){
         return res.status(400).json({message: "Password must be at least 6 characters long"})
     }
@@ -19,7 +23,15 @@ export const signup = async (req,res)=>{
         password: hashedPassword
     })
     if(newUser){
+        generateToken(newUser._id,res)
+        await newUser.save()
+        return res.status(201).json({
+            _id: newUser._id,
+            fullName: newUser.fullName,
+            email: newUser.email,
+            profilePic:newUser.profilePic
 
+        })
     }
     else{
         return res.status(400).json({message: "Invalid user data"})
@@ -27,6 +39,8 @@ export const signup = async (req,res)=>{
 
    }
    catch(error){
+    console.log("Error in signup controller:",error.message)
+    return res.status(500).json({message: "Internal server error"})
 
    }
 }
