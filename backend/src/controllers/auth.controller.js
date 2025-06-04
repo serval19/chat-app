ce166948bf28cd665b93c4c49d7399879818e499
serval19 <diwatomy04@gmail.com>
2025-06-04 19:58:44 +0530

    auth routes completed. message routes yet to implement

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -98,3 +98,12 @@ export const updateProfile = async (req,res)=>{
     }
     
 }
+export const checkAuth= (req,res)=>{
+    try {
+        return res.status(200).json(req.user)
+    } catch (error) {
+        console.log("Error in checkAuth controller:",error.message)
+        return res.status(500).json({message: "Internal server error"})
+        
+    }
+}
