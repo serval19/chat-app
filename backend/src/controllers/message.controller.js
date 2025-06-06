6ec4e1def3a83f0bf83a4f3c832e8bfc0f9cb8e1
serval19 <diwatomy04@gmail.com>
2025-06-06 21:01:35 +0530

    message route partially implemented

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,34 @@
+import Message from "../models/message.model.js"
+import User from "../models/user.model.js"
+
+export const getUsersForSidebar=async (req,res)=>{
+    try {
+        const loggedInUserId=req.user._id
+        const filteredUsers=await User.find({_id:{$ne:loggedInUserId}}).select("-password")
+        return res.status(200).json(filteredUsers)
+    } catch (error) {
+        console.error("Error fetching users for sidebar:", error.message);
+        return res.status(500).json({ message: "Internal server error" });
+        
+    }
+
+}
+
+export const getMessages=async (req,res)=>{
+    
+    try {
+        const {id:userToChatId}=req.params
+        const myId=req.user._id
+        const messages=await Message.find({
+            $or:[
+                {senderId:myId,receiverId:userToChatId},
+                {senderId:userToChatId,receiverId:myId}
+            ]
+        })
+        return res.status(200).json(messages)
+    } catch (error) {
+        console.error("Error fetching messages:", error.message);
+        return res.status(500).json({ message: "Internal server error" });
+        
+    }
+}
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cookieParser from 'cookie-parser'
 import authRoutes from './routes/auth.route.js'
+import messageRoutes from './routes/message.route.js'
 import { connectDB } from "./lib/db.js";
 import dotenv from 'dotenv'
 import { protectRoute } from './middleware/auth.middleware.js';
@@ -11,6 +12,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use("/api/auth",authRoutes)
+app.use("/api/message",messageRoutes)
 app.listen(5001,()=>{
     console.log('Server is running on port 50001')
     connectDB();
