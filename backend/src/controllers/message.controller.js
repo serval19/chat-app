b0521e09d1c2ea563bb9dda000a71245bbd2f2a8
serval19 <diwatomy04@gmail.com>
2025-06-07 20:28:37 +0530

    backend almost ready. socket.io yet to be implemented.

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import Message from "../models/message.model.js"
 import User from "../models/user.model.js"
+import cloudinary from "../lib/cloudinary.js"
 
 export const getUsersForSidebar=async (req,res)=>{
     try {
@@ -28,6 +29,36 @@ export const getMessages=async (req,res)=>{
         return res.status(200).json(messages)
     } catch (error) {
         console.error("Error fetching messages:", error.message);
+        return res.status(500).json({ message: "Internal server error" });
+        
+    }
+}
+
+export const sendMessage=async (req,res)=>{
+    try {
+        const {text,image}=req.body
+        const {id: receiverId}=req.params
+        const senderId=req.user._id
+        //if user sends an image, it is uploaded to cloudinary
+        let imageUrl
+        if(image){
+            //upload base 64 image to cloudinary
+            const uploadResponse= await cloudinary.uploader.upload(image)
+            imageUrl=uploadResponse.secure_url
+
+        }
+        const newMessage=new Message({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl
+        })
+        await newMessage.save()
+        //todo : realtime functionality goes here => socket.io
+        return res.status(201).json(newMessage)
+    
+    } catch (error) {
+        console.error("Error in message controller", error.message);
         return res.status(500).json({ message: "Internal server error" });
         
     }
