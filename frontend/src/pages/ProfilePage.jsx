cef0448ddc1fca1748dbc937417c1930f1382ed4
serval19 <diwatomy04@gmail.com>
2025-06-22 20:51:34 +0530

    edit profile page completed

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useAuthStore } from '../store/useAuthStore'
 import { Camera, Mail, User } from 'lucide-react'
 
 const ProfilePage = () => {
   const { authUser,isUpdatingProfile,updateProfile} = useAuthStore()
-  const handleImageUpload = async(e)=>{}
+  const [selectedImage, setSelectedImage]=useState(null)
+  const handleImageUpload = async(e)=>{
+    const file= e.target.files[0]
+    if(!file) return
+
+    const reader=new FileReader()
+    reader.readAsDataURL(file)
+    reader.onload=async()=>{
+      const base64Image=reader.result
+      setSelectedImage(base64Image)
+      await updateProfile({profilePic: base64Image})
+    }
+  }
   return (
     <div className='h-screen pt-20'>
       <div className='max-w-2xl mx-auto p-4 py-8'>
@@ -18,7 +30,7 @@ const ProfilePage = () => {
           <div className="flex flex-col items-center gap-4">
             <div className="relative">
               <img
-                src={authUser.profilePic || "/avatar.png"}
+                src={selectedImage||authUser.profilePic || "/avatar.png"}
                 alt="Profile"
                 className="size-32 rounded-full object-cover border-4 "
               />
@@ -63,6 +75,19 @@ const ProfilePage = () => {
                 Email Address
               </div>
               <p className="px-4 py-2.5 bg-base-200 rounded-lg border">{authUser?.email}</p>
+            </div>
+          </div>
+          <div className="mt-6 bg-base-300 rounded-xl p-6">
+            <h2 className="text-lg font-medium  mb-4">Account Information</h2>
+            <div className="space-y-3 text-sm">
+              <div className="flex items-center justify-between py-2 border-b border-zinc-700">
+                <span>Member Since</span>
+                <span>{authUser.createdAt?.split("T")[0]}</span>
+              </div>
+              <div className="flex items-center justify-between py-2">
+                <span>Account Status</span>
+                <span className="text-green-500">Active</span>
+              </div>
             </div>
           </div>
        
