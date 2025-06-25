27064591cd42a9dadd73c9b61f3997efcf18ba97
serval19 <diwatomy04@gmail.com>
2025-06-25 20:14:18 +0530

    settings page partially done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,9 +10,11 @@ import { useAuthStore } from './store/useAuthStore'
 import { Loader } from 'lucide-react'
 import { Navigate } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
+import { useThemeStore } from './store/useThemeStore'
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore()
+  const {theme}=useThemeStore()
   
   useEffect(() => {
     checkAuth()
@@ -31,7 +33,7 @@ const App = () => {
   
 
   return (
-    <div data-theme="retro">
+    <div data-theme={theme}>
       <Navbar />
       <Routes>
         {/* Protected routes */}
