f0b8773e00693c76e47d3c2fcde0305186e2310f
serval19 <diwatomy04@gmail.com>
2025-06-30 20:29:41 +0530

    sidebar partially completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ app.use(cors({
 }))
 
 app.use("/api/auth",authRoutes)
-app.use("/api/message",messageRoutes)
+app.use("/api/messages",messageRoutes)
 app.listen(5001,()=>{
     console.log('Server is running on port 50001')
     connectDB();
