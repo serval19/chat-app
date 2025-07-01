77d6ffcb74a425377fe007b2a744141eba9819d2
serval19 <diwatomy04@gmail.com>
2025-07-01 23:59:16 +0530

    chat container partial implementation

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { useChatStore } from '../store/useChatStore'
+import { useEffect } from 'react'
 
 const ChatContainer = () => {
+  const {messages,getMessages,isMessagesLoading,selectedUser}=useChatStore()
+  useEffect(()=>{
+    getMessages(selectedUser._id)
+  },[selectedUser._id,getMessages])
+  if(isMessagesLoading) return <div>Loading..</div>
   return (
-    <div>ChatContainer</div>
+    <div className='flex-2 flex flex-col overflow-auto'>
+      <ChatHeader />
+    </div>
   )
 }
 
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -2,10 +2,11 @@ import React, { useEffect } from 'react'
 import { useChatStore } from '../store/useChatStore'
 import SidebarSkeleton from './skeletons/SidebarSkeleton';
 import { Users } from 'lucide-react';
+import { useAuthStore } from '../store/useAuthStore';
 
 const Sidebar = () => {
   const { getUsers, users,selectedUser,setSelectedUser,isUsersLoading}=useChatStore();
-  const onlineUsers=[]
+  const { onlineUsers}=useAuthStore()
   useEffect(()=>{
     getUsers()
   },[getUsers])
