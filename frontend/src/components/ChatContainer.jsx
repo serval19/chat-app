19bfaa088c8f171d5edb22c1ea591936d086fb58
serval19 <diwatomy04@gmail.com>
2025-07-02 22:20:25 +0530

    chatcontainer partially done

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
 import { useChatStore } from '../store/useChatStore'
 import { useEffect } from 'react'
+import ChatHeader from './ChatHeader'
+import MessageInput from './MessageInput'
+import MessageSkeleton from './skeletons/MessageSkeleton'
 
 const ChatContainer = () => {
   const {messages,getMessages,isMessagesLoading,selectedUser}=useChatStore()
   useEffect(()=>{
     getMessages(selectedUser._id)
   },[selectedUser._id,getMessages])
-  if(isMessagesLoading) return <div>Loading..</div>
+  if(isMessagesLoading) return (
+    <div className='flex-1 flex flex-col overflow-auto'>
+      <ChatHeader />
+      <MessageSkeleton />
+      <MessageInput />
+
+    </div>
+  )
   return (
     <div className='flex-2 flex flex-col overflow-auto'>
       <ChatHeader />
+      <p>messages...</p>
+      <MessageInput />
     </div>
   )
 }
