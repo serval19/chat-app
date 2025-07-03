3b20d7ba12282c257e2e47056dcac9d54419dba9
serval19 <diwatomy04@gmail.com>
2025-07-03 22:20:14 +0530

    message input field partially done. updated the store to handle message sending

--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { useChatStore } from '../store/useChatStore'
+import { X } from 'lucide-react'
 
 const MessageInput = () => {
+  const [text, setText]=useState("")
+  const [imagePreview, setImagePreview]=useState(null)
+  const fileInputRef=useRef(null)
+  const {sendMessage}=useChatStore()
+  const handleImageChange=(e)=>{};
+  const removeImage=()=>{};
+  const handleSendMessage= async(e)=>{}
+
   return (
-    <div>MessageInput</div>
+    <div className='p-4 w-full'>
+       {imagePreview && (
+        <div className="mb-3 flex items-center gap-2">
+          <div className="relative">
+            <img
+              src={imagePreview}
+              alt="Preview"
+              className="w-20 h-20 object-cover rounded-lg border border-zinc-700"
+            />
+            <button
+              onClick={removeImage}
+              className="absolute -top-1.5 -right-1.5 w-5 h-5 rounded-full bg-base-300
+              flex items-center justify-center"
+              type="button"
+            >
+              <X className="size-3" />
+            </button>
+          </div>
+        </div>
+      )}
+
+
+    </div>
   )
 }
 
