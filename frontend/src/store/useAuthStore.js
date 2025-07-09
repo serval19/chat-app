08b4995770d33391de2d0cc048c48103e5a69cfc
serval19 <diwatomy04@gmail.com>
2025-07-09 21:53:39 +0530

    connect socket and disconnect socket functions

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,6 +1,9 @@
 import {create} from 'zustand'
 import {axiosInstance} from '../lib/axios.js'
 import {toast} from 'react-hot-toast'
+import { io } from 'socket.io-client'
+const BASE_URL= "http://localhost:5001"
+
 
 export const useAuthStore= create((set,get)=>({
     authUser: null,
@@ -89,8 +92,15 @@ export const useAuthStore= create((set,get)=>({
             set({isUpdatingProfile: false})
         }
     },
-    connectSocket: ()=>{},
-    disconnectSocket: ()=>{}
+    connectSocket: ()=>{
+        const {authUser}=get()
+        if(!authUser || get().socket?.connected) return
+        const socket=io(BASE_URL)
+        socket.connect()
+    },
+    disconnectSocket: ()=>{
+        if(get().socket?.connected) get().socket.disconnect()
+    }
 
 })
 )
